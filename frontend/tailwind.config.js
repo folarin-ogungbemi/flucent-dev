dbd1cab80153394157e6af91e19b6e4c8b430bc4
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-02 23:43:43 +0200

    Create color palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,31 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      textColor:{
+        skin:{
+          pry_text:'var(--pry-col)',
+          sec_text:'var(--sec-col)',
+          link:'var(--links)',
+          link_hover:'var(--links-hover)',
+          btnText:'var(--button-text-col)',
+        }
+      },
+      borderColor:{
+        skin:{
+          section_divide:'var(--section-divide)'
+        }
+      },
+       backgroundColor:{
+        skin:{
+          button_bg_pry:'var(--button-bg-pry-col)',
+          button_bg_sec:'var(--button-bg-sec-col)',
+          pry_bg:'var(--pry-col)',
+          sec_bg:'var(--sec-col)',
+          
+        }
+    },
+    },
   },
   plugins: [],
 }
