763a0ea766e85280943a5ce7f0d38b8e452a7845
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-02 23:35:52 +0200

    Create Page Layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App:React.FC = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </Router>
+   
   )
 }
 
