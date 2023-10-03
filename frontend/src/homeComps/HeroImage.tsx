56fa2e54b46c70df1af6f5d0092fface3ed94f78
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-03 23:49:32 +0200

    Add svg image to hero

--- frontend/src/homeComps/HeroImage.tsx
+++ frontend/src/homeComps/HeroImage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+// https://undraw.co/illustrations
+import image from '../assets/Images/programming.svg'
 
 const HeroImage:React.FC = () => {
   return (
     <div className='py-5'>
         <div className=''>
-            This Image
+            <img src={image} alt="developer's image" className='w-[80%] h-[70%] mx-auto'/>
         </div>
     </div>
   )
