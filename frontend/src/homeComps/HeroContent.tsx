00491f7c16e3e9fb0f5b9e70ecf68fa3e764e7d3
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-03 15:46:01 +0200

    Create home components

---
+++ frontend/src/homeComps/HeroContent.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {AiOutlineArrowRight} from 'react-icons/ai'
+
+const Hero:React.FC = () => {
+  return (
+   <div className='flex py-5'>
+        <div className='flex flex-col space-y-5'>
+            <h1 className='text-5xl font-bold text-start w-[90%]'>Mastercraft in Building Frontend & Backend Aplications.</h1>
+            <p className='text-md text-start w-[80%]'>Leveraging technology with aesthetic design & software architecture that exhibits an amazing digital experience.</p>
+            <div className='flex space-x-5'>
+                <button className='rounded-full py-4 px-5 border bg-skin-button_bg_sec text-skin-pry_text hover:drop-shadow-lg'>Contact Us</button>
+                <button className='rounded-full py-4 px-5 border flex hover:drop-shadow-lg'>Projects <AiOutlineArrowRight className='ml-2 my-auto'/></button>
+            </div>
+        </div>
+  </div>
+  )
+}
+
+export default Hero;
---
+++ frontend/src/homeComps/HeroImage.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroImage:React.FC = () => {
+  return (
+    <div className='py-5'>
+        <div className=''>
+            This Image
+        </div>
+    </div>
+  )
+}
+
+export default HeroImage;
