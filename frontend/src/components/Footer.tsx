7ee4cc646d446829a305efd9ee1aae4866e73575
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-03 15:46:35 +0200

    Create Footer content

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Footer:React.FC = () => {
+  return (
+    <div className='w-full'>
+        <div className='container mx-auto p-4 flex justify-between text-sm'>
+            <Link to='' className='hover:underline font-semibold text-skin-link hover:text-skin-link_hover'>Site map</Link>
+            <Link to='' className='font-semibold'>&copy;2023 <span className='text-skin-link hover:text-skin-link_hover hover:underline'>FlucentDev.com</span> | All Rights reserved.</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Footer;
