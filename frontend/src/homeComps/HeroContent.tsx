f5a38e7dede64a4111d9678b1b35bba8b6a63d31
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-03 23:49:51 +0200

    Add margin bottom

--- frontend/src/homeComps/HeroContent.tsx
+++ frontend/src/homeComps/HeroContent.tsx
@@ -5,7 +5,7 @@ const Hero:React.FC = () => {
   return (
    <div className='flex py-5'>
         <div className='flex flex-col space-y-5'>
-            <h1 className='text-5xl font-bold text-start w-[90%]'>Mastercraft in Building Frontend & Backend Aplications.</h1>
+            <h1 className='text-5xl font-bold mb-5 text-start w-[90%]'>Mastercraft in Building Frontend & Backend Aplications.</h1>
             <p className='text-md text-start w-[80%]'>Leveraging technology with aesthetic design & software architecture that exhibits an amazing digital experience.</p>
             <div className='flex space-x-5'>
                 <button className='rounded-full py-4 px-5 border bg-skin-button_bg_sec text-skin-pry_text hover:drop-shadow-lg'>Contact Us</button>
