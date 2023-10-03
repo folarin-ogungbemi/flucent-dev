c52b921cc8b65a80425024084ef500b467b70b83
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-03 15:47:38 +0200

    Structure Home Component

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
-import Hero from '../homeComps/hero';
+import Hero from '../homeComps/HeroContent';
+import Image from '../homeComps/HeroImage'
 import Layout from '../components/Layout'
+import Footer from '../components/Footer'
 
 const Home:React.FC = () => {
   return (
     <Layout title='FlucentDev | Home' content='FlucentDev Homepage'>
-      <Hero/>
+      <div className='w-full h-[78.95dvh] bg-skin-complement_bg flex justify-center items-center'>
+        <div className='container mx-auto px-4 grid grid-cols-2 my-auto'>
+          <Hero/>
+          <Image/>
+        </div>
+      </div>
+      <Footer/>
     </Layout>
   )
 }
