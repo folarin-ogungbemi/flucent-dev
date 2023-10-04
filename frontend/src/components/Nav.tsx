5fd541d99933c55b3be57c79f554d9efb895f6c1
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-04 17:31:12 +0200

    Add Home link to Nav

--- frontend/src/components/Nav.tsx
+++ frontend/src/components/Nav.tsx
@@ -3,13 +3,13 @@ import { NavLink } from "react-router-dom";
 
 const Nav:React.FC = () => {
 
-  const navLinks:Array<string> = ['studio', 'services', 'work', 'experience', 'contact', 'blog']
+  const navLinks:Array<string> = ['home', 'studio', 'services', 'work', 'process', 'contact', 'blog']
 
   return (
     <nav className='flex space-x-5'>
       {navLinks.map((nav, navIndex)=>
         <NavLink
-            to={`/${nav}`}
+            to={nav==='home'? '/':`/${nav}`}
             key={navIndex}
             className='capitalize font-medium h-fit underline-nav-link text-skin-link hover:text-skin-link_hover'>
             {nav}
