fdc5903e35c1e423d26718f6cf605f8d8776c3ae
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-04 17:18:17 +0200

    Create studio about component

---
+++ frontend/src/studioComps/About.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const About:React.FC = () => {
+  return (
+    <>
+        <h2 className='text-5xl font-light'>Love, Design & Development.</h2>
+        <hr className='mt-3 mb-1'/>
+        <small>Founded in Feb 2023 at the heart of Berlin, Germany.</small>
+        <section className='my-5'>
+            <h3 className='text-2xl font-medium'>Our Story</h3>
+            <p>FlucentDev studio was born in my old apartment...</p>
+        </section>
+    </>
+  )
+}
+
+export default About;
