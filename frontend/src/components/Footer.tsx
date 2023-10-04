97f0eae326d915b2d691bbaafd4d6b119eb3d8fd
folarin-ogungbemi <ftogungbemi@gmail.com>
2023-10-04 17:32:36 +0200

    Add border to footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import {Link} from 'react-router-dom'
 
 const Footer:React.FC = () => {
   return (
-    <div className='w-full'>
+    <div className='w-full border-t-2 border-skin-section_divide z-40'>
         <div className='container mx-auto p-4 flex justify-between text-sm'>
             <Link to='' className='hover:underline font-semibold text-skin-link hover:text-skin-link_hover'>Site map</Link>
             <Link to='' className='font-semibold'>&copy;2023 <span className='text-skin-link hover:text-skin-link_hover hover:underline'>FlucentDev.com</span> | All Rights reserved.</Link>
